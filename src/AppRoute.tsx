b879827bc4b095b3d072d8ce15e580f0888afa0e
dishasah <dishasah6@gmail.com>
2024-06-20 18:22:50 +0530

    dishh9

--- src/AppRoute.tsx
+++ src/AppRoute.tsx
@@ -15,7 +15,7 @@ const AppRoute : React.FC  = () => {
 
     <Routes> 
         <Route path=""element = { <HomePage/>} />
-        <Route path="student "element = { <StudentOutlet/>} >
+        <Route path="student" element = { <StudentOutlet/>} >
         <Route path=""element = { <Students/>} />
         <Route path="create-student"element = { <CreateStudent/>} />
         <Route path=" update-student/: studentID "element = { <UpdateStudent/>} />
--- src/pages/Students.tsx
+++ src/pages/Students.tsx
@@ -1,8 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import{ IStudent } from "../interface/student";
+import { Link } from "react-router-dom";
 
-const Students : React.FC = () => {
-  return <> update </>;
-  
+import "./Students.scss";
+
+const Students: React.FC = () => {
+
+  const [allStudents, setAllStudents ] = useState<IStudent[]>();
+  useEffect(()=>{
+    const student = JSON.parse(localStorage.getItem("students")as string);
+    setAllStudents(student)
+console.log("student data",allStudents);
+  },[]);
+
+
+ 
+  return( 
+  <> 
+  <button title="Create Student" className="create_button">
+    <Link to="/student/create-student">Create Student</Link>
+  </button>
+  <table>
+        <thead>
+          <tr>
+            <td>SN.</td>
+            <td>Name</td>
+            <td>Age</td>
+            <td>Phone Number</td>
+            <td>Email ID</td>
+          </tr>
+        </thead>
+        <tbody>
+          {allStudents?.map((item, index) => {
+            return (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{item.name}</td>
+                <td>{item.age}</td>
+                <td>{item.phone}</td>
+                <td>{item.email}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+  <br />
+  <hr />
+     
+  </>
+  );
 };
 
-export default  Students ;
+export default Students;
